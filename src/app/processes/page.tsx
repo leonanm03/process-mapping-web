e0cae84753b49f54a60f73368cdc7b70a5bd7c81
Leonan Martins <leonanm04@gmail.com>
2023-09-20 19:14:48 -0300

    feat: cascade layout test

---
+++ src/app/processes/page.tsx
@@ -0,0 +1,24 @@
+export default function Processes() {
+  return (
+    <div className="row">
+      <ul className="list-disc pl-5">
+        <li>pai</li>
+        <li>pai</li>
+        <li>pai</li>
+        <li>pai</li>
+        <ul className="list-disc pl-5 mt-2">
+          <li>filho</li>
+          <li>filho</li>
+          <li>filho</li>
+          <li>filho</li>
+          <ul className="list-disc pl-5 mt-2">
+            <li>neto</li>
+            <li>neto</li>
+            <li>neto</li>
+            <li>neto</li>
+          </ul>
+        </ul>
+      </ul>
+    </div>
+  );
+}
