aaa56743fb8366aa83ecc24c42c041258bd9a6b8
Leonan Martins <leonanm04@gmail.com>
2023-09-20 15:57:37 -0300

    feat: navbar layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+export function Navbar() {
+  return (
+    <div className="navbar bg-base-200">
+      <div className="navbar-start">
+        <a className="btn btn-ghost normal-case text-xl" href="/">
+          <img src="./logo.png" alt="logo" />
+          Process Mapping
+        </a>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a>Link 1</a>
+            </li>
+            <li>
+              <a>Link 2</a>
+            </li>
+            <li>
+              <a>Link 3</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
