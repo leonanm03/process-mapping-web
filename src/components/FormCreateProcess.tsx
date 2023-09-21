2eb1ddf8043fc565a5cbfb3a6138711729df16c1
Leonan Martins <leonanm04@gmail.com>
2023-09-21 16:10:22 -0300

    fix: areas from api

--- src/components/FormCreateProcess.tsx
+++ src/components/FormCreateProcess.tsx
@@ -1,8 +1,7 @@
-import {
-  CreateProcess,
-  CreateProcessForm,
-  HandleChangeFields,
-} from "@/protocols";
+"use client";
+import { Area, CreateProcessForm, HandleChangeFields } from "@/protocols";
+import { getAreas } from "@/services";
+import { useEffect, useState } from "react";
 
 export function FormCreateProcess({
   body,
@@ -10,6 +9,8 @@ export function FormCreateProcess({
   handleSubmit,
   children,
 }: CreateProcessForm) {
+  const [areas, setAreas] = useState<Area[]>([]);
+
   function handleChange(e: HandleChangeFields) {
     setBody({ ...body, [e.target.name]: e.target.value });
   }
@@ -17,6 +18,13 @@ export function FormCreateProcess({
   function handleAreaIdChange(e: React.ChangeEvent<HTMLSelectElement>) {
     setBody({ ...body, [e.target.name]: parseInt(e.target.value) });
   }
+
+  useEffect(() => {
+    getAreas().then((areas) => {
+      setBody({ ...body, areaId: areas[0].id });
+      setAreas(areas);
+    });
+  }, []);
 
   return (
     <div className="card flex-shrink-0 w-full max-w-2xl shadow-2xl bg-base-100">
@@ -46,10 +54,11 @@ export function FormCreateProcess({
             onChange={handleAreaIdChange}
             defaultValue={body.areaId}
           >
-            <option value={1}>Area 1</option>
-            <option value={2}>Area 2</option>
-            <option value={3}>Area 3</option>
-            <option value={4}>Area 4</option>
+            {areas.map((area) => (
+              <option key={area.id} value={area.id}>
+                {area.name}
+              </option>
+            ))}
           </select>
         </div>
 
--- src/protocols/index.js
+++ src/protocols/index.js
@@ -2,3 +2,4 @@ export * from "./create-process-protocol";
 export * from "./handle-change-fields-protocol";
 export * from "./get-process-protocol";
 export * from "./create-process-form-protocol";
+export * from "./area";
