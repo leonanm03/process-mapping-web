2f325ab5b54db71c37368045d04824b48ee3bcd9
Leonan Martins <leonanm04@gmail.com>
2023-09-20 21:04:32 -0300

    feat: cascade layout collapse prototype

--- src/app/processes/page.tsx
+++ src/app/processes/page.tsx
@@ -19,17 +19,61 @@ export default function Processes() {
   }, []);
 
   return (
-    <div className="row">
-      <ul className="list-disc pl-5">
-        {processes.map((process) => (
-          <li key={process.id}>
-            {process.name}
-            {process.subProcess.map((subprocess) => (
-              <SubprocessCascade key={subprocess.id} {...subprocess} />
-            ))}
-          </li>
-        ))}
-      </ul>
-    </div>
+    // <div className="row">
+    //   <ul className="list-disc pl-5">
+    //     {processes.map((process) => (
+    //       <li key={process.id}>
+    //         {process.name}
+    //         {process.subProcess.map((subprocess) => (
+    //           <SubprocessCascade key={subprocess.id} {...subprocess} />
+    //         ))}
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
+
+    <main className="flex justify-center p-6">
+      <div className="collapse bg-base-300 ">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">
+          Click me to show/hide content
+        </div>
+        <div className="collapse-content">
+          <div className="collapse bg-base-300 max-w-max">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Click me to show/hide content
+            </div>
+            <div className="collapse-content">
+              <div className="collapse bg-base-300 max-w-max">
+                <input type="checkbox" />
+                <div className="collapse-title text-xl font-medium">
+                  Click me to show/hide content
+                </div>
+                <div className="collapse-content">
+                  <div className="collapse bg-base-300 max-w-max">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl font-medium">
+                      Click me to show/hide content
+                    </div>
+                    <div className="collapse-content">
+                      <div className="collapse bg-base-300 max-w-max">
+                        <input type="checkbox" />
+                        <div className="collapse-title text-xl font-medium">
+                          Click me to show/hide content
+                        </div>
+                        <div className="collapse-content">
+                          <p>hello</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
